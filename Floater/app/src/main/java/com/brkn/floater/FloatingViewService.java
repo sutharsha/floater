fcf2caeb1d10d50e96d89d5b48f7f384e67912b0
Sutharshan Ram <sutharsha@gmail.com>
2018-09-01 21:38:39 +0530

    migrated project from aide to android studio, added accessibility code(wip)

---
+++ Floater/app/src/main/java/com/brkn/floater/FloatingViewService.java
@@ -0,0 +1,220 @@
+package com.brkn.floater;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class FloatingViewService extends Service {
+
+    final String MIMETYPE_TEXT_PLAIN = "text/plain";
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Inflate the floating view layout we created
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+        //Add the view to the window.
+        //        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        //                WindowManager.LayoutParams.WRAP_CONTENT,
+        //                WindowManager.LayoutParams.WRAP_CONTENT,
+        //                WindowManager.LayoutParams.TYPE_PHONE,
+        //                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+        //                PixelFormat.TRANSLUCENT);
+
+        int LAYOUT_FLAG;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        //Specify the view position
+        params.gravity = Gravity.TOP | Gravity.START;        //Initially view will be added to top-left corner
+        params.x = 0;
+        params.y = 100;
+
+        //Add the view to the window
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+        //The root element of the collapsed view layout
+        final View collapsedView = mFloatingView.findViewById(R.id.collapse_view);
+        //The root element of the expanded view layout
+        final View expandedView = mFloatingView.findViewById(R.id.expanded_container);
+
+        //Set the close button
+        ImageView closeButtonCollapsed = (ImageView) mFloatingView.findViewById(R.id.close_btn);
+        closeButtonCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close the service and remove the from from the window
+                stopSelf();
+            }
+        });
+
+        //Set the view while floating view is expanded.
+        //Set the play button.
+        ImageView playButton = (ImageView) mFloatingView.findViewById(R.id.play_btn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing the song.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Set the next button.
+        ImageView nextButton = (ImageView) mFloatingView.findViewById(R.id.next_btn);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            //private String MIMETYPE_TEXT_PLAIN = "text/*";
+
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(FloatingViewService.this, "Playing next song.", Toast.LENGTH_LONG).show();
+
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                String pasteData = "grf";
+
+                // If it does contain data, decide if you can handle the data.
+                if (!(clipboard.hasPrimaryClip())) {
+
+                } else if (!(clipboard.getPrimaryClipDescription().hasMimeType(MIMETYPE_TEXT_PLAIN))) {
+
+                    // since the clipboard has data but it is not plain text
+
+                } else {
+
+                    //since the clipboard contains plain text.
+                    ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
+
+                    // Gets the clipboard as text.
+                    pasteData = item.getText().toString();
+                    Toast.makeText(FloatingViewService.this, pasteData + "Playing next song.", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        //Set the pause button.
+        ImageView prevButton = (ImageView) mFloatingView.findViewById(R.id.prev_btn);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing previous song.", Toast.LENGTH_LONG).show();
+                startService(new Intent(FloatingViewService.this, SampleAccessibilityService.class));
+            }
+        });
+
+        //Set the close button
+        ImageView closeButton = (ImageView) mFloatingView.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+            }
+        });
+
+        //Open the application on thi button click
+        ImageView openButton = (ImageView) mFloatingView.findViewById(R.id.open_button);
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open the application  click.
+                Intent intent = new Intent(FloatingViewService.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                //close the service and remove view from the view hierarchy
+                stopSelf();
+            }
+        });
+
+        //Drag and move floating view using user's touch action.
+        mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+
+                        //get the touch location
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        int Xdiff = (int) (event.getRawX() - initialTouchX);
+                        int Ydiff = (int) (event.getRawY() - initialTouchY);
+
+                        //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
+                        //So that is click event.
+                        if (Xdiff < 10 && Ydiff < 10) {
+                            if (isViewCollapsed()) {
+                                //When user clicks on the image view of the collapsed layout,
+                                //visibility of the collapsed layout will be changed to "View.GONE"
+                                //and expanded view will become visible.
+                                collapsedView.setVisibility(View.GONE);
+                                expandedView.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Detect if the floating view is collapsed or expanded.
+     *
+     * @return true if the floating view is collapsed.
+     */
+    private boolean isViewCollapsed() {
+        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null) {
+            mWindowManager.removeView(mFloatingView);
+        }
+    }
+}
---
+++ Floater/app/src/main/java/com/brkn/floater/SampleAccessibilityService.java
@@ -0,0 +1,39 @@
+package com.brkn.floater;
+
+import android.accessibilityservice.AccessibilityService;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+/**
+ * SampleAccessibilityService
+ *
+ * @author sutharsha
+ */
+public class SampleAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        AccessibilityNodeInfo source = accessibilityEvent.getSource();
+        if (source != null) {
+            AccessibilityNodeInfo rowNode = getRootInActiveWindow();
+            if (rowNode != null) {
+                for (int i = 0; i < rowNode.getChildCount(); i++) {
+                        AccessibilityNodeInfo accessibilityNodeInfo = rowNode.getChild(i);
+                    if (accessibilityNodeInfo.isEditable()){
+                        if(accessibilityNodeInfo.isFocused()) {
+                            accessibilityNodeInfo.performAction(AccessibilityNodeInfoCompat.ACTION_PASTE);
+                            return;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
